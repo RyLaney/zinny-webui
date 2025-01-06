4ef63c4da8c463593ddfacf067f163ae11c70415
TeusML <ML@TEUS.media>
2025-01-06 15:50:01 -0800

    removes debug output

---
+++ pyinstaller/hooks/hook-zinny_api.py
@@ -0,0 +1,5 @@
+from PyInstaller.utils.hooks import collect_data_files
+
+datas = collect_data_files('zinny_api')
+# print("zinny_api data files:")
+# print(datas)
---
+++ pyinstaller/hooks/hook-zinny_surveys.py
@@ -0,0 +1,5 @@
+from PyInstaller.utils.hooks import collect_data_files
+
+datas = collect_data_files('zinny_surveys')
+# print("zinny_surveys data files:")
+# print(datas)
