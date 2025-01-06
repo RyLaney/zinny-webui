a1b0d116ce013d94420683ed5edd1103e5c3333c
TeusML <ML@TEUS.media>
2025-01-06 15:49:37 -0800

    better words

--- platypus/zinny-launch-webui.bash
+++ platypus/zinny-launch-webui.bash
@@ -4,15 +4,23 @@ echo "Starting Zinny Web UI"
 echo "After the server starts, the web app"
 echo "will be available at http://localhost:7219"
 echo
-echo 'Close this terminal to stop the server'
+echo "App was packaged with Platypus. https://sveinbjorn.org/platypus"
+echo "Binary was built with PyInstaller. https://pyinstaller.org/"
 echo
-echo "For faster launching, run the script directly from the terminal"
+echo "Your ratings are exportable and also stored in:"
+echo "~/Library/Application\ Support/zinny/db/zinny-1.0.sqlite'"
+echo
+echo 'Close the app window to stop the server'
+echo 
+echo "Launching the app bundle takes several seconds, you will see output"
+echo "below when its ready."
+echo
+echo "You may run the script directly from the terminal or install with pip"
 echo "/Applications/Zinny\ Launch\ WebUI.app/Contents/Resources/zinny-webui"
 echo
 echo "More info at https://github.com/RyLaney/zinny-webui"
-echo 
-echo "App was packaged with Platypus"
-echo "Executable was packaged with PyInstaller"
 echo
+echo "Launching server..."
+echo 
 
 ./zinny-webui
